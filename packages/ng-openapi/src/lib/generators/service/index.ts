38b829d27158585d4cb792f463fc3f670736a5fc
tareq <tareq@thejami.com>
2025-07-19 12:44:06 +0200

    feat: add BaseInterceptorGenerator to core generator and update generation logic

--- packages/ng-openapi/src/lib/generators/service/index.ts
+++ packages/ng-openapi/src/lib/generators/service/index.ts
@@ -1,4 +1,4 @@
 export * from "./service.generator";
 export * from "./service-method.generator";
 export * from "./service-index.generator";
-export * from "./service-method/index";
+export * from "./service-method";
