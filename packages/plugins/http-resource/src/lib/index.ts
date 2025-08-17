6662403a8ec7a120835c8daee7173b6a46939e07
tareq <tareq@thejami.com>
2025-08-17 10:52:53 +0200

    chore(http-resource): remove unused exports from index.ts

--- packages/plugins/http-resource/src/lib/index.ts
+++ packages/plugins/http-resource/src/lib/index.ts
@@ -1,4 +1,1 @@
 export { HttpResourceGenerator as HttpResourcePlugin } from "./http-resource.generator";
-export * from "./http-resource-method.generator";
-export * from "./http-resource-index.generator";
-export * from "./http-resource-method";
