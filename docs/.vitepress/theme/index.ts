80f955039bcb753cd594804ed2aac22a6d1ecd44
tareq <tareq@thejami.com>
2025-08-18 11:32:03 +0200

    docs: removed custom css

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,5 @@
 import DefaultTheme from 'vitepress/theme'
-import './custom.css'
 
 export default {
-  extends: DefaultTheme,
-  enhanceApp({ app, router, siteData }) {
-    // App level enhancements
-    // You can register global components here
-  }
+  extends: DefaultTheme
 }
